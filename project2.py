eeea15c110ac438f26d83d45c9e933a667821047
Tigran Meliksetyants <tigran338@gmail.com>
2023-04-22 13:55:03 -0700

    Server part(In progress)

--- project2.py
+++ project2.py
@@ -1,12 +1,21 @@
-
 import sys
+import socket
+import threading
+import sys
+import time
 
+#Server connected variables
+connections = []
+
+
+#Distance vector Algoritm variables
 max_int32 = 2 ** 31 - 1
 myid = None
 servers = None
 row_table = None
 dv_table = {}
-edgesid = {}
+
+
 
 
 def read_topology(file_path):
@@ -20,7 +29,6 @@ def read_topology(file_path):
             server_id, ip, port = file.readline().strip().split()
             servers[int(server_id)] = {'ip': ip, 'port': int(port)}
 
-        # Initialize dv_table with 'inf' for all cells except the current server
 
         neighbor = set()
         row_table = set()
@@ -76,16 +84,79 @@ def update_dv_table(neighbor_id:int, neighborTable:dict):
         dv_table[myid][server_id] = min(dv_table[myid][server_id], dv_table[myid][neighbor_id] + dv_table[neighbor_id][server_id])
     
     
-    
-    
 
+def start_server():
+    pass   
+
+
+
+
+#SERVER PART
+def accept_connections():
+    global servers
+    port = servers[myid]['port']
+    ip = servers[myid]['ip']
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.bind((ip, port))
+    server_socket.listen(5)
+    print(f"Server started and listening on port {port}")
+    while True:
+        client_socket, client_address = server_socket.accept()
+        connections.append((client_socket, client_address))
+        print(f"New connection from {client_address}")
+        client_thread = threading.Thread(target=handle_client, args=(client_socket, client_address))
+        client_thread.start()
+
+def handle_client(client_socket, client_address):
+    while not exit:
+        try:
+            data = client_socket.recv(1024)
+        except:
+            return
+        if not data:
+            connections.remove((client_socket, client_address))
+            print(f"Connection closed: {client_address}")
+            break
+        else:
+            if data.decode() == "Close Connection":
+                connections.remove((client_socket,client_address))
+                print(f"Connection with {client_address} terminated")
+            else:
+                print(f"Received message from {client_address}: {data.decode()}")
+
+def send_message(connection_id, message):
+    connection = connections[connection_id]
+    connection[0].sendall(message.encode())
+    print(f"message sent to: {connection_id}")
+
+def connect_to(address, port):
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket.connect((address, port))
+    connections.append((client_socket, (address, port)))
+    print(f"Connected to {address}:{port}")
+    client_thread = threading.Thread(target=handle_client, args=(client_socket, (address, port)))
+    client_thread.start()
+
+
+def connect_to_neighbors():
+    
+    for neighbor_id in row_table:
+        try:
+            if myid != neighbor_id:
+                connect_to(servers[neighbor_id]['ip'] , servers[neighbor_id]['port'])
+                print(str(neighbor_id))
+        except:
+            print(f"Was no able to connect to {neighbor_id} server") 
+
+#Main part
 if __name__ == "__main__":
     # Read topology from file and store in servers and edges
     read_topology('topology.txt')
 
     # Display the topology
     display_dv_table()
-
+    connect_to_neighbors()
+    accept_connections()
     while True:
         neighbor_id = int(input("Enter neighbor_id: "))
         num_servers = len(servers)
